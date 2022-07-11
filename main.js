1b0d841e587bf66925e6705391a48b4460f3688f
Willian <willmmarques@hotmail.com>
2022-07-10 21:46:33 -0300

    implementação de um condicional

--- main.js
+++ main.js
@@ -5,31 +5,31 @@ function autonomiaAutomotiva(){
   let valorCombustivel = parseFloat(document.getElementById('precoCombustivel').value);
   
   let kmPorLitro = parseFloat(document.getElementById('kmPorLitro').value);
+
+  if(valorAbastecido && valorCombustivel && kmPorLitro){
+    let calculo = (valorAbastecido / valorCombustivel)*kmPorLitro;
   
-  let calculo = (valorAbastecido / valorCombustivel)*kmPorLitro;
-
-  let elementoCalculo = document.getElementById("resultado");
-
-  let autonomia = calculo
-
-  elementoCalculo.innerHTML = autonomia;
+    let elementoCalculo = document.getElementById("resultado");
+  
+    let autonomia = calculo
+  
+    elementoCalculo.innerHTML = autonomia;
+  
+    //litros
+  
+    let calculo1 = (valorAbastecido / valorCombustivel);
+  
+    let elementoCalculo1 = document.getElementById("litro");
+  
+    let litros = calculo1
+  
+    elementoCalculo1.innerHTML = litros;
+  }else{
+    alert("Os campo precisa ser preenchido")
+  }
+  
 
 };
-function litros(){
-  let valorAbastecido = parseFloat(document.getElementById('valorAbastecido').value);
-  
-  let valorCombustivel = parseFloat(document.getElementById('precoCombustivel').value);
-  
-  let kmPorLitro = parseFloat(document.getElementById('kmPorLitro').value);
-  
-  let calculo = (valorAbastecido / valorCombustivel);
 
-  let elementoCalculo = document.getElementById("litro");
-
-  let litros = calculo
-
-  elementoCalculo.innerHTML = litros;
-
-};
 
 
